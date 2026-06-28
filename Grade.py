60b3b678c7e59199737d2e070291ef38c057bb47
Aswathi8387 <pandian311983@gmail.com>
2026-06-28 15:32:39 +0530

    Add grading system based on input marks

---
+++ Grade.py
@@ -0,0 +1,13 @@
+mark=int(input("Enter a mark:"))
+if mark>=90:
+  print("A grade")
+elif mark>=80:
+  print("B grade")
+elif mark>=70:
+  print("C grade")
+elif mark>=60:
+  print("D grade")
+elif mark>=50:
+  print("E grade")
+else:
+  print("Fail")
